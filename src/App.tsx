2fcccd729b186de81ece1fc1f42b69d0d97e79ca
CauaBraga04 <kakabraga.cb.cb.cb@gmail.com>
2025-05-25 23:13:49 -0700

    In the beginning

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import TopNavComponent from './Components/TopNavComponent';
+
+function App() {
+  return (
+   <>
+   <TopNavComponent></TopNavComponent>
+   </>
+  );
+}
+
+export default App;
---
+++ src/Components/TopNavComponent.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function TopNavComponent() {
+  return (
+    <>
+    <div id="Main">
+      <h1>Grocery List Manager</h1>
+    </div>
+    </>
+  )
+};
+
+export default TopNavComponent;
