bbb75b2426a602590da151ea71e3d683416a141b
CauaBraga04 <kakabraga.cb.cb.cb@gmail.com>
2025-05-26 22:09:43 -0700

    1

--- src/Components/TopNavComponent.tsx
+++ src/Components/TopNavComponent.tsx
@@ -1,13 +1,30 @@
 import React from "react";
+import Button from "@mui/material/Button";
+import { Drawer } from "@mui/material";
+
 
 function TopNavComponent() {
+  const [open, setOpen] = React.useState(false);
+
+  const toggleDrawer = (newOpen: boolean) => () => {
+    setOpen(newOpen);
+  };
+
   return (
     <>
-    <div id="Main">
-      <h1>Grocery List Manager</h1>
-    </div>
+      <div id="Main">
+        <h1>Grocery List Manager</h1>
+      </div>
+      <button onClick={toggleDrawer(true)}>Open drawer</button>
+      <Drawer open={open} onClose={toggleDrawer(false)}>
+        Hello, this is the drawer content!
+        <br />
+        <Button>Primary</Button>
+        <br />
+        <Button>Secondary</Button>
+      </Drawer>
     </>
-  )
-};
+  );
+}
 
 export default TopNavComponent;
